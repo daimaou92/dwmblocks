888a97e760aa724102e46fee664f37202c5931da
Daimaou92 <daimaou92@mail>
2020-06-04 00:54:33 +0530

    merged with ignore-empty patch

---
+++ config.h
@@ -0,0 +1,16 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",30,0},
+
+	{"VOL: ", "amixer sget Master | egrep -e \"[1-9]+[0-9]?[0-9]?\%\" -o | head -n1", 0, 10},
+
+	{"BRT: ", "xbacklight | sed 's/\\..*//'",0,11},
+	
+	{"TEMP: ", "sensors | awk '/^temp1:/{print $2}'",5,0},
+
+	{"", "date \"+%a %d %b %Y %I:%M:%S %p\"", 45, 0}
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
