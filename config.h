10968827c431378539410d57e34939f7814013a2
Daimaou92 <daimaou92@mail>
2020-06-06 12:47:25 +0530

    Network manager wifi connection block

--- config.h
+++ config.h
@@ -3,13 +3,15 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",30,0},
 
-	{"VOL: ", "amixer sget Master | egrep -e \"[1-9]+[0-9]?[0-9]?\%\" -o | head -n1", 0, 10},
+	{"VOL: ", "amixer sget Master | egrep -e \"[1-9]+[0-9]?[0-9]?\%\" -o | head -n1",0,10},
 
 	{"BRT: ", "xbacklight | sed 's/\\..*//'",0,11},
 	
 	{"TEMP: ", "sensors | awk '/^temp1:/{print $2}'",5,0},
 
-	{"", "date \"+%a %d %b %Y %I:%M:%S %p\"", 45, 0}
+	{"WIFI: ", "$SCRIPT_DIR/wifi.sh",30,0},
+
+	{"", "date \"+%a %d %b %Y %H:%M:%S\"",1,0}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
