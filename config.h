414cdf3364140a3b22193e9d40d405f64639cf5f
Daimaou92 <daimaou92@mail>
2020-06-17 11:45:30 +0530

    changed to emojis

--- config.h
+++ config.h
@@ -3,17 +3,17 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "$DWMBLOCKS_SCRIPTS_DIR/has_updates.sh",120,0},
 
-	{"MEM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",30,0},
+	{"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",30,0},
 
-	{"VOL: ", "amixer sget Master | egrep -e \"[1-9]+[0-9]?[0-9]?\%\" -o | head -n1",0,10},
+	{"", "$DWMBLOCKS_SCRIPTS_DIR/volume.sh",0,10},
 
-	{"BRT: ", "xbacklight | sed 's/\\..*//'",0,11},
+	{"🔆 ", "xbacklight | sed 's/\\..*//'",0,11},
 	
-	{"TEMP: ", "sensors | awk '/^temp1:/{print $2}'",5,0},
+	{"🌡 ", "sensors | awk '/^temp1:/{print $2}'",5,0},
 
-	{"WIFI: ", "$DWMBLOCKS_SCRIPTS_DIR/wifi.sh",30,0},
+	{"📶 ", "$DWMBLOCKS_SCRIPTS_DIR/wifi.sh",30,0},
 
-	{"BAT: ", "echo \"$(cat /sys/class/power_supply/BAT0/capacity)% [$(cat /sys/class/power_supply/BAT0/status)]\"",2,0},
+	{"🔋 ", "echo \"$(cat /sys/class/power_supply/BAT0/capacity)% [$(cat /sys/class/power_supply/BAT0/status)]\"",2,0},
 
 	{"", "date \"+%a %d %b %Y %H:%M:%S\"",1,0}
 };
