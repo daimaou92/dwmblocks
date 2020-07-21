da771c392d88f7197c7a7d6ec363cb159635ab4a
Daimaou92 <daimaou92@mail>
2020-07-21 12:05:06 +0530

    Removed wifi in favour of nm-applet

--- config.h
+++ config.h
@@ -3,6 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{" ", "$DWMBLOCKS_SCRIPTS_DIR/has_updates.sh",120,0},
 
+	{"", "$DWMBLOCKS_SCRIPTS_DIR/wttr.sh",1800,0},
+
 	{"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",30,0},
 
 	{"", "$DWMBLOCKS_SCRIPTS_DIR/volume.sh",0,10},
@@ -11,7 +13,7 @@ static const Block blocks[] = {
 	
 	{"🌡 ", "sensors | awk '/^temp1:/{print $2}'",5,0},
 
-	{"📶 ", "$DWMBLOCKS_SCRIPTS_DIR/wifi.sh",30,0},
+	// {"📶 ", "$DWMBLOCKS_SCRIPTS_DIR/wifi.sh",30,0},
 
 	{"🔋 ", "echo \"$(cat /sys/class/power_supply/BAT0/capacity)% [$(cat /sys/class/power_supply/BAT0/status)]\"",2,0},
 
